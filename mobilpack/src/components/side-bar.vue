7033de3bf4ebe62dceac73fdbba37792080e3d09
Lee Hwayong <dlghkdyd500@gmail.com>
2021-01-18 18:43:16 +0900

    add css for li (아직 완성안됐습니다~)

--- mobilpack/src/components/side-bar.vue
+++ mobilpack/src/components/side-bar.vue
@@ -1,10 +1,10 @@
 <template>
     <div class = 'v-sidebar-menu'>
         <ul id='menu'>
-            <li>프로필 변경</li>
-            <li>회원 관리</li>
-            <li>추천 장소 관리</li>
-            <li>서비스관리</li>
+            <li class='vsm--link vsm--link_level-2'>프로필 변경</li>
+            <li class='vsm--link vsm--link_level-2'>회원 관리</li>
+            <li class='vsm--link vsm--link_level-2'>추천 장소 관리</li>
+            <li class='vsm--link vsm--link_level-2'>서비스관리</li>
         </ul>
     </div>
 </template>
