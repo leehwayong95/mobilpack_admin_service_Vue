cf144afd6e53878946e0f13303682a8a619451a0
Lee Hwayong <dlghkdyd500@gmail.com>
2021-01-20 13:58:30 +0900

    add method for pwupdate function

---
+++ mobilpack/src/components/alert/editPwModal.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="modal">
+        <div class="modal_message">
+            <input type="password" v-model='currentpw' placeholder="현재 비밀번호">
+            <input type="password" v-model='editpw' placeholder="변경 비밀번호">
+            <input type="password" v-model='confirmpw' placeholder="변경 비밀번호 확인">
+        </div>
+        <div class="modal_button_wrap">
+          <input class="btn btn-default col-md-3" @click="editPw" type="button" value="변경">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      currentpw: '',
+      editpw: '',
+      confirmpw: ''
+    }
+  },
+  props: [
+    'hot_table'
+  ],
+  methods: {
+    editPw () {
+      if (this.currentpw === '' || this.editPw === '' || this.confirmpw === '') {
+        alert('비밀번호를 입력해주세요.')
+      } else {
+        if (this.editpw === this.confirmpw) {
+          this.$axios.post('http://localhost:9000/api/su/my/pwupdate', {
+            currentpw: this.currentpw,
+            editpw: this.editpw
+          })
+            .then((res) => {
+              if (res.status === 202) {
+                alert('현재 비밀번호와 일치하지 않습니다.')
+              } else {
+                alert('변경되었습니다.')
+                this.$emit('close')
+              }
+            })
+        } else {
+          alert('새비밀번호와 일치하지않습니다.\n다시 입력해주세요. ')
+          this.editpw = ''
+          this.confirmpw = ''
+        }
+      }
+    }
+  }
+}
+</script>
--- mobilpack/src/components/topbar.vue
+++ mobilpack/src/components/topbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id='top'>
       <span>{{route}}</span>
-      <span><a href="/myinfo">안녕하세요 {{name}}님</a></span>
+      <span><a v-on:click="editinfo">안녕하세요 {{name}}님</a></span>
       <button type="button" v-on:click="logout" >로그아웃</button>
   </div >
 </template>
@@ -24,12 +24,14 @@ export default {
         alert('로그인이 필요합니다.')
         this.$router.push('/')
       }
-      // mounted라 한번밖에 실행안됨 고쳐야함
     },
     logout () {
       this.$cookie.delete('name')
       this.$cookie.delete('authorization')
       this.$router.push('/')
+    },
+    editinfo () {
+      this.$router.push('/myinfo')
     }
   }
 }
