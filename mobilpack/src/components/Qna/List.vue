c3a6c4307e73eb04bb37902c8e23243d4b3d9b45
Lee Hwayong <dlghkdyd500@gmail.com>
2021-02-02 12:08:33 +0900

    질문 작성 회원 삭제시 표시가 안되던 버그 해결

--- mobilpack/src/components/Qna/List.vue
+++ mobilpack/src/components/Qna/List.vue
@@ -71,7 +71,8 @@
             <td v-else-if="post.category == 3">기타</td>
           <td class="long">{{post.title}}</td>
           <td>{{post.createat}}</td>
-          <td>{{post['user_name']}}</td>
+            <td v-if="post['user_name'] == null">삭제된 회원</td>
+            <td v-else>{{post['user_name']}}</td>
             <td v-if="post.replydate == null">답변대기</td>
             <td v-else>답변완료</td>
             <td v-if="(post['admin_name'] == null ) && (post.replydate == null)"> - </td>
