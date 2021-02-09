ae85e9827e02ecff410321000ec98299fcdd8e37
Lee Hwayong <dlghkdyd500@gmail.com>
2021-02-09 11:22:27 +0900

    route에 따른 sidebar item 활성화 표시

--- mobilpack/src/components/side-bar.vue
+++ mobilpack/src/components/side-bar.vue
@@ -5,8 +5,24 @@
             <li><span v-on:click="jump('myinfo')">프로필 변경</span></li>
             <li><span v-on:click="jump('user')">회원 관리</span></li>
             <li><span v-on:click="jump('recommands')">추천 장소 관리</span></li>
-            <li><span>서비스관리</span>
-                <ul>
+            <li>
+              <span v-if="route == 'details' || route == 'Qna' || route =='Notice'" style="background: #3e61dc;">서비스관리</span>
+              <span v-else>서비스관리</span>
+                <ul v-if="route == 'details' || route == 'Qna' || route =='Notice'" style="display: block;">
+                    <li>
+                      <span v-if="route == 'details'" v-on:click="jump('details')" style="color: #3e61dc;">관리자 관리</span>
+                      <span v-else v-on:click="jump('details')">관리자 관리</span>
+                    </li>
+                    <li>
+                      <span v-if="route == 'Qna'" v-on:click="jump('qna')" style="color: #3e61dc;">문의 관리</span>
+                      <span v-else v-on:click="jump('qna')">문의 관리</span>
+                    </li>
+                    <li>
+                      <span v-if="route == 'Notice'" v-on:click="jump('notice')" style="color: #3e61dc;">공지사항 관리</span>
+                      <span v-else v-on:click="jump('notice')">공지사항 관리</span>
+                    </li>
+                </ul>
+                <ul v-else>
                     <li><span v-on:click="jump('details')">관리자 관리</span></li>
                     <li><span v-on:click="jump('qna')">문의 관리</span></li>
                     <li><span v-on:click="jump('notice')">공지사항 관리</span></li>
@@ -20,12 +36,13 @@
 export default {
   data () {
     return {
-      fuck: null
+      route: this.$route.name
     }
   },
   methods: {
     jump (s) {
       this.$router.push('/' + s)
+      this.route = this.$route.name
     }
   }
 }
