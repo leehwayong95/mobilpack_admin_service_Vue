01765758da41ce7c41dec69da90521274842cc5d
Lee Hwayong <dlghkdyd500@gmail.com>
2021-02-09 11:40:14 +0900

    경로에 맞춰 활성화 되어있는 Css 적용 및 router push를 name으로 적용

--- mobilpack/src/components/side-bar.vue
+++ mobilpack/src/components/side-bar.vue
@@ -1,10 +1,19 @@
 <template>
     <div class = 'v-sidebar-menu'>
         <ul id='menu'>
-            <li class='logo' v-on:click="jump('main')">T-Mas<br>Admin Service</li>
-            <li><span v-on:click="jump('myinfo')">프로필 변경</span></li>
-            <li><span v-on:click="jump('user')">회원 관리</span></li>
-            <li><span v-on:click="jump('recommands')">추천 장소 관리</span></li>
+            <li class='logo' v-on:click="jump('admin')">T-Mas<br>Admin Service</li>
+            <li>
+              <span v-if="route == 'myinfo'" style="background: #3e61dc;" v-on:click="jump('myinfo')">프로필 변경</span>
+              <span v-else v-on:click="jump('myinfo')">프로필 변경</span>
+            </li>
+            <li>
+              <span v-if="route == 'userList'" style="background: #3e61dc;" v-on:click="jump('userList')">회원 관리</span>
+              <span v-else v-on:click="jump('userList')">회원 관리</span>
+            </li>
+            <li>
+              <span v-if="route == 'recommnads'" style="background: #3e61dc;" v-on:click="jump('recommands')">추천 장소 관리</span>
+              <span v-else v-on:click="jump('recommands')">추천 장소 관리</span>
+            </li>
             <li>
               <span v-if="route == 'details' || route == 'Qna' || route =='Notice'" style="background: #3e61dc;">서비스관리</span>
               <span v-else>서비스관리</span>
@@ -14,18 +23,18 @@
                       <span v-else v-on:click="jump('details')">관리자 관리</span>
                     </li>
                     <li>
-                      <span v-if="route == 'Qna'" v-on:click="jump('qna')" style="color: #3e61dc;">문의 관리</span>
-                      <span v-else v-on:click="jump('qna')">문의 관리</span>
+                      <span v-if="route == 'Qna'" v-on:click="jump('Qna')" style="color: #3e61dc;">문의 관리</span>
+                      <span v-else v-on:click="jump('Qna')">문의 관리</span>
                     </li>
                     <li>
-                      <span v-if="route == 'Notice'" v-on:click="jump('notice')" style="color: #3e61dc;">공지사항 관리</span>
-                      <span v-else v-on:click="jump('notice')">공지사항 관리</span>
+                      <span v-if="route == 'Notice'" v-on:click="jump('Notice')" style="color: #3e61dc;">공지사항 관리</span>
+                      <span v-else v-on:click="jump('Notice')">공지사항 관리</span>
                     </li>
                 </ul>
                 <ul v-else>
                     <li><span v-on:click="jump('details')">관리자 관리</span></li>
-                    <li><span v-on:click="jump('qna')">문의 관리</span></li>
-                    <li><span v-on:click="jump('notice')">공지사항 관리</span></li>
+                    <li><span v-on:click="jump('Qna')">문의 관리</span></li>
+                    <li><span v-on:click="jump('Notice')">공지사항 관리</span></li>
                 </ul>
             </li>
         </ul>
@@ -41,8 +50,10 @@ export default {
   },
   methods: {
     jump (s) {
-      this.$router.push('/' + s)
-      this.route = this.$route.name
+      if (this.route !== s) {
+        this.$router.push({name: s})
+        this.route = this.$route.name
+      }
     }
   }
 }
