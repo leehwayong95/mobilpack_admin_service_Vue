72017e1141014068d3ecde410c1e6ff6f40e8ee3
Lee Hwayong <dlghkdyd500@gmail.com>
2021-02-17 16:46:00 +0900

    npm install 필요합니다 (vue multiselect 추가)

--- mobilpack/src/components/Recommand/List.vue
+++ mobilpack/src/components/Recommand/List.vue
@@ -1,94 +1,123 @@
 <template>
   <div id="content">
-    <div>
-      <naver-maps
-        :height="height"
-        :width="width"
-        :mapOptions="mapOptions"
-        :initLayers="initLayers"
-        @load="onLoad">
-      </naver-maps>
+    <div class="title">
+      <h1>| 추천 장소 관리</h1>
+      <h3>HOME > 서비스관리 > 추천장소관리</h3>
     </div>
-    <div>
-      <input class="input-text" type="text" v-model="testOne" placeholder='위도'>
-      <input class="input-text" type="text" v-model="testTwo" placeholder='경도'>
+    <div class="search">
+      <ul>
+        <li>
+          <span>카테고리</span>
+          <select v-model="tmp.category">
+            <option value="">전체</option>
+            <option value="관광지">관광지</option>
+            <option value="숙소">숙소</option>
+            <option value="맛집">맛집</option>
+          </select>
+        </li>
+        <li class="lang" style="width: 26%;">
+          <span>등록언어</span>
+          <multiselect
+            v-model="tmp.selecttag"
+            label="name"
+            :options="language"
+            track-by="value"
+            :multiple="true"
+            :tagable="true"
+            @tag="addLanguageOption"
+            :close-on-select="false"
+            placeholder="">
+            <template template slot="selection">
+            </template>
+          </multiselect>
+        </li>
+        <li class ="date" style="width: 370px">
+          <span>서비스상태</span>
+        </li>
+        <li>
+          <span>추천장소명</span>
+        </li>
+      </ul>
+      <button @click="test">검색</button>
     </div>
-    <button @click="mapinput">submit</button>
-    <button @click="pop">지도 선택</button>
-    <div>
-      <naver-map
-          :height="height"
-          :width="width"
-          :mapOptions="mapOptions"
-          :initLayers="initLayers"
-          @load="onLoad">
-        </naver-map>
+    <div class="cont_inner">
+      <div class="title">
+        <span>| 검색결과</span>
+        <button>신규등록</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Multiselect from 'vue-multiselect'
 export default {
-  name: 'HelloWorld',
+  components: {
+    Multiselect
+  },
+  mounted () {
+  },
   data () {
     return {
-      width: 400,
-      height: 400,
-      info: false,
-      marker: null,
-      count: 1,
-      map: null,
-      isCTT: false,
-      testOne: '',
-      testTwo: '',
-      setCenter: '',
-      mapOptions: { /* 제주 시청을 기본값으로 설정함 */
-        lat: 33.49959,
-        lng: 126.53126,
-        zoom: 16,
-        zoomControl: true,
-        zoomControlOptions: {position: 'TOP_RIGHT'},
-        mapTypeControl: true
+      tmp: {
+        category: '',
+        language: '',
+        selecttag: []
       },
-      initLayers: ['BACKGROUND', 'BACKGROUND_DETAIL', 'POI_KOREAN', 'TRANSIT', 'ENGLISH', 'CHINESE', 'JAPANESE']
-    }
-  },
-  computed: {
-    hello () {
-      return `Hello, World! × ${this.count}`
+      Search: {
+        category: '',
+        language: '',
+        selecttag: []
+      },
+      language: [
+        {name: '한국어', value: '1'},
+        {name: '영어', value: '2'},
+        {name: '일본어', vaule: '4'},
+        {name: '중국어', value: '8'}
+      ],
+      value: []
     }
   },
   methods: {
-    onLoad (vue) {
-      this.map = vue
+    addLanguageOption (newTag) {
+      let value = ''
+      switch (newTag) {
+        case ('한국어'):
+          value = 1
+          break
+        case ('영어'):
+          value = 2
+          break
+        case ('일본어'):
+          value = 4
+          break
+        case ('중국어'):
+          value = 8
+          break
+      }
+      const tag = {
+        name: newTag,
+        value: value
+      }
+      this.language.push(tag)
+      this.tmp.selecttag.push(tag)
     },
-    onWindowLoad (that) {
-    },
-    onMarkerClicked (event) {
-      this.info = !this.info
-    },
-    onMarkerLoaded (vue) {
-      this.marker = vue.marker
-    },
-    mapinput () {
-      this.map.setCenter({lat: this.testOne, lng: this.testTwo})
-    },
-    pop () {
-      window.open('http://localhost:8080/pop', '_blank')
+    test () {
+      console.log(this.tmp.language)
     }
-  },
-  mounted () {
-    setInterval(() => this.count++, 1000)
   }
 }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
 <style scoped>
-.info-window-container {
-  padding: 10px;
-  width: 300px;
-  height: 100px;
-  }
-.input-text{
-  width: 200px;
+div.search ul {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+div.search Multiselect{
+  position: absolute;
 }
 </style>
