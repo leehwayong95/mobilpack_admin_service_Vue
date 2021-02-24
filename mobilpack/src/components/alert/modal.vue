a9acbb286325a884c3f657a0492e39b3e9e1a700
Lee Hwayong <dlghkdyd500@gmail.com>
2021-02-24 12:27:38 +0900

    공용 css에 개인 css 제거 (modal, login 컴포넌트 관련) Modal (로그인 alert) 안내 메시지 변경, 닫기 버튼 추가 Login 컴포넌트 안내 메시지 추가 및 기획서에 맞게 css 변경

--- mobilpack/src/components/alert/modal.vue
+++ mobilpack/src/components/alert/modal.vue
@@ -1,15 +1,41 @@
 <template>
-    <div id="modal">
-        <div class="modal_message">
-            <span>
-               로그인에 실패하였습니다.<br>로그인 정보를 다시 확인해주세요.
-            </span>
-        </div>
-        <div class="modal_button_wrap">
-          <input class="btn btn-default col-md-3" @click="$emit('close')" type="button" value="확인">
-        </div>
+  <div id="modal">
+    <div class="closebtn" @click="$emit('close')">
+        <img src="..\..\assets\images\btn_modalclose.png" width="20px">
+      </div>
+    <div class="modal_message">
+      <span>
+        미등록 아이디입니다.<br><br>확인 후 다시 입력해주세요.
+      </span>
     </div>
+    <div class="modal_button_wrap">
+      <input class="btn btn-default col-md-3" @click="$emit('close')" type="button" value="확인">
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.modal_message {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  margin-bottom: 10px;
+  margin-top: 20px;
+  padding: 10px;
+}
+div.closebtn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+div.modal_button_wrap input{
+  cursor: pointer;
+  width: 100px;
+  height: 30px;
+}
+</style>
+
 <script>
 export default {
   data () {
